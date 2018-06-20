d7ea5df08dcfff6f7676a4366e52b11cbdb86ee5
changqingshang <scqcs@qq.com>
2018-06-20 17:17:52 +0800

    update eureka

--- plat-eureka/src/main/java/com/scq/plat/Application.java
+++ plat-eureka/src/main/java/com/scq/plat/Application.java
@@ -2,14 +2,14 @@ package com.scq.plat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@EnableAutoConfiguration
 @EnableEurekaServer
-@RestController
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
